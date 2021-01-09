600dd9eba6d75b7bba40c8dd92afe7ad7f78caa1
Cymbelmine <74141947+Cymbelmine@users.noreply.github.com>
2021-01-09 10:54:15 -0600

    Attempt at forecast function
    Deletion of forecast-dummy in HTML
    Attempt at generating forecast function in JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-let now = new Date();
-let dateTime = document.querySelector("small");
-let date = now.getDate();
-let minutes = (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
-let time = now.getHours() + ":" + minutes;
-let year = now.getFullYear();
+function formatDate (timestamp) {
+    let date = new Date(timestamp);
+    
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+return `${day} ${formatHours(timestamp)}`;
+}
 
-let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-let day = days[now.getDay()];
+function formatHours(timestamp){
+      let date = new Date(timestamp);
+    let hours = date.getHours();
+     if (hours < 10){
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`;
+}
 
 let months = [
   "Jan",
@@ -52,6 +63,26 @@ icon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weath
 icon.setAttribute("alt", response.data.weather[0].description);
 }
 
+function showForecast(response){
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = null;
+let forecast = null;
+
+for (let index = 0; index < 6; index++) {
+forecast = response.data.list[index];
+forecastElement.innerHTML += `<div class="col-2">
+        <h3>
+            ${formatHours(forecast.dt * 1000)}
+        </h3>
+        <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" />
+        <div class="weather-forecast-temperature">
+            <strong>${Math.round(forecast.main.temp_max)}º 
+            </strong> 
+            ${Math.round(forecast.main.temp_min)}º
+        </div>
+    </div>`;    
+}
+}
 function searchCity(city) {
   let apiKey = "747707261d2068249e8900a1bcf9371c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -68,6 +99,23 @@ let city = document.querySelector("#city");
       searchInput.value.charAt(0).toUpperCase() + searchInput.value.slice(1)
     }`;
  searchCity(searchInput.value);
+}
+
+function showFahrenheitTemperature(event){
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+celsiusLink.classList.remove("active");
+FahrenheitLink.classList.add("active");
+let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+temperature.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function showCelsiusTemperature(event){
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let form = document.querySelector("#searchCityForm");
